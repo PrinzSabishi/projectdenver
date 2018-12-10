5d081c1edba7ed5b5f330406dff7aced839176fc
prinzsabishi <msfysabishi@gmail.com>
2018-12-10 15:21:00 +0100

    Update smc.js

--- js/smc.js
+++ js/smc.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
         function losujObr(x) {
             return Math.floor(1+Math.random()*(x));
         }
+        function losujNrMsg(x) {
+            return Math.floor(1+Math.random()*(x-1));
+        }
+
+
 
         var nrob = losujObr(6);
         var nrpostac = losujObr(3);
@@ -16,19 +21,32 @@ $(document).ready(function(){
         else {
             var postac = "taihou";
         }
-
-        var postacmsg = "";
+        var nrmsg = losujNrMsg(3);
+        var postacmsg = [];
         switch (postac) {
             case "clev":
-                postacmsg = "My sister Cleveland is for sure the coolest person I've ever known!";
+                postacmsg = [
+                    "My sister Cleveland is for sure the coolest person I've ever known!",
+                    "This is Cleveland! She is my sister. One day, I want to become just like her!",
+                    "For sure you heard about Cleveland! Who didn't?! She is sooo cool. Am I right?"
+                ] ;
+                
                 break;
             
             case "rafi":
-                postacmsg = "She is called Laffey. It's the most calmest person I've ever known.. but also one of the coolest! Did you saw her while latest battle was going on?!";
+                postacmsg = [
+                    "She is called Laffey. It's the most calmest person I've ever known.. but also one of the coolest! Did you saw her while latest battle was going on?!",
+                    "That's Laffey! She is suuuper cute and always sleepy!",
+                    "That girl here is called Laffey. At first she seems to be calm and innocent but believe me.. that's a little devil while in battle!"
+                ];
                 break;
 
             case "taihou":
-                postacmsg = "That woman her is called Taihou. She is the hardest aircraft carrier in Azur Lane. If you have some luck you might see her beautiful and destructive formation of planes.";
+                postacmsg = [
+                    "That woman here is called Taihou. She is the hardest aircraft carrier in Azur Lane. If you have some luck you might see her beautiful and destructive formation of planes.",
+                    "Meet Taihou! I'm always impresed by her strength!",
+                    "Oh! Look, that's Taihou - The Hardest Aircraft Carrier in IJN. This is so cool!"
+                ];
                 break;
 
             default:
@@ -36,7 +54,7 @@ $(document).ready(function(){
         }
         var imgurl = "img/"+postac+nrob+".jpg";
         console.log(imgurl);
-        $("#queryContainerID").append("<div class='queryBox animated fadeInRight'><p>"+postacmsg+"</p></div>");
+        $("#queryContainerID").append("<div class='queryBox animated fadeInRight'><p>"+postacmsg[nrmsg]+"</p></div>");
         $("#queryContainerID").append("<div class='queryBox animated fadeInRight'><img src='"+imgurl+"' class='img-fluid' /></div>");
 
     });
